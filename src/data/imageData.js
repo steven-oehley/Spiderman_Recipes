5195c8cca3aa4f3c77fb9cc6eb1e072e26bc625c
Steve <steve.gitprofile@outlook.com>
2024-07-21 16:28:39 +0200

    prepared project and added images and data

---
+++ src/data/imageData.js
@@ -0,0 +1,35 @@
+import spiderStew from "../assets/img/spider-sense-stew.webp";
+import spiderPizza from "../assets/img/spider-pizza.webp";
+import spiderSalad from "../assets/img/spider-salad.webp";
+import spiderBurger from "../assets/img/spider-burger.webp";
+import spiderPancakes from "../assets/img/spider-pancakes.webp";
+import spiderStirFry from "../assets/img/spider-stir-fry.webp";
+import spiderSmoothie from "../assets/img/spider-smoothie.webp";
+import spiderWrap from "../assets/img/spider-wrap.webp";
+import spiderEnchilada from "../assets/img/spider-enchilada.webp";
+import spiderBurrito from "../assets/img/spider-bfast-burrito.webp";
+import spiderPlatter from "../assets/img/spider-platter.webp";
+import spiderFalaffel from "../assets/img/spider-falafel.webp";
+import spiderWaffle from "../assets/img/spider-waffle.webp";
+import spiderSpaghetti from "../assets/img/spider-spaghetti.webp";
+import spiderPeppers from "../assets/img/spider-peppers.webp";
+import spiderPastas from "../assets/img/spider-pasta.webp";
+
+export const imageMap = {
+  1: spiderStew,
+  2: spiderPizza,
+  3: spiderSalad,
+  4: spiderBurger,
+  5: spiderPasta,
+  6: spiderPancakes,
+  7: spiderStirFry,
+  8: spiderSmoothie,
+  9: spiderWrap,
+  10: spiderEnchilada,
+  11: spiderBurrito,
+  12: spiderPlatter,
+  13: spiderFalaffel,
+  14: spiderWaffle,
+  15: spiderSpaghetti,
+  16: spiderPeppers,
+};
